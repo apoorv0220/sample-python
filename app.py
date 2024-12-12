ae281fa94813994e47dfde0464cbd8e614b353ca
apoorv0220 <apoorv0220@gmail.com>
2024-12-12 12:30:09 +0530

    Fix requirements

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS from flask_cors
 import json
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from pdf2image import convert_from_path
-from pytesseract import image_to_string
-from docx import Document
+# from pdf2image import convert_from_path
+# from pytesseract import image_to_string
+# from docx import Document
 
 app = Flask(__name__)
 CORS(app)
@@ -15,16 +15,16 @@ tokenizer = AutoTokenizer.from_pretrained("numind/NuExtract-1.5-tiny", trust_rem
 model.to("cpu")
 model.eval()
 
-def pdf_to_text(file_path):
-    pages = convert_from_path(file_path, 500)
-    text = ""
-    for page in pages:
-        text += image_to_string(page)
-    return text
+# def pdf_to_text(file_path):
+#     pages = convert_from_path(file_path, 500)
+#     text = ""
+#     for page in pages:
+#         text += image_to_string(page)
+#     return text
 
-def docx_to_text(file_path):
-    doc = Document(file_path)
-    return "\n".join([para.text for para in doc.paragraphs])
+# def docx_to_text(file_path):
+#     doc = Document(file_path)
+#     return "\n".join([para.text for para in doc.paragraphs])
 
 def predict_NuExtract(model, tokenizer, text, schema, examples=[""]):
     input_llm = "<|input|>\n### Template:\n" + schema + "\n"
@@ -46,13 +46,13 @@ def extract():
     input_text = data.get("text")
     schema = data.get("schema")
     examples = data.get("examples", [""])
-    is_pdf = data.get("is_pdf", False)
-    is_docx = data.get("is_docx", False)
+    # is_pdf = data.get("is_pdf", False)
+    # is_docx = data.get("is_docx", False)
 
-    if is_pdf:
-        input_text = pdf_to_text(input_text)
-    elif is_docx:
-        input_text = docx_to_text(input_text)
+    # if is_pdf:
+    #     input_text = pdf_to_text(input_text)
+    # elif is_docx:
+    #     input_text = docx_to_text(input_text)
 
     prediction = predict_NuExtract(model, tokenizer, input_text, schema, examples)
     return jsonify({"prediction": prediction})
